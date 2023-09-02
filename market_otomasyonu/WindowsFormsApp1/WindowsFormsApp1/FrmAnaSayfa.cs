7887476da9ceee7971dbf6289c9538a36b4978cf
Eray Sirdas <sirdaseray.bjk@gmail.com>
2023-09-03 01:00:05 +0300

    market_otomasyon
    Orta düzey bir market otomasyonu

---
+++ market_otomasyonu/WindowsFormsApp1/WindowsFormsApp1/FrmAnaSayfa.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class FrmAnaSayfa : Form
+    {
+        public FrmAnaSayfa()
+        {
+            InitializeComponent();
+        }
+        SqlConnection veri = new SqlConnection("Data Source=RESULCANKIRTI;Initial Catalog=market;Integrated Security=True");
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FrmSatis d = new FrmSatis();
+            d.Show();
+           this.Hide();
+           // veri.Open();
+            //MessageBox.Show("bağlantı kuruldu...");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+           FrmBölgemüdürü n = new FrmBölgemüdürü();
+            n.Show();
+            this.Hide();
+        }
+    }
+}
